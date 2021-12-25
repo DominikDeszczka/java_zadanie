5bfd14af2d05701c00892fc617db5d68cb767a01
DominikDeszczka <92813525+DominikDeszczka@users.noreply.github.com>
2021-12-25 23:20:38 +0100

    kolejna zmiana

--- src/zadanie/main.java
+++ src/zadanie/main.java
@@ -3,6 +3,6 @@ package zadanie;
 public class main {
 
     public static void main(String[] args){
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD__");
     }
 }
